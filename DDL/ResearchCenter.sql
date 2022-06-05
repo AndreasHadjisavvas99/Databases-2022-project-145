919aa183dc1edf7f4653e6420dba14829a45e8b8
LoukiaPavlana <el18711@mail.ntua.gr>
2022-06-05 21:48:57 +0300

    commit DDL

---
+++ DDL/ResearchCenter.sql
@@ -0,0 +1,15 @@
+insert into ResearchCenter (ID_resCe, GovBudg, PrivBudg) values (1, 56, 32);
+insert into ResearchCenter (ID_resCe, GovBudg, PrivBudg) values (2, 46, 65);
+insert into ResearchCenter (ID_resCe, GovBudg, PrivBudg) values (3, 92, 59);
+insert into ResearchCenter (ID_resCe, GovBudg, PrivBudg) values (4, 25, 85);
+insert into ResearchCenter (ID_resCe, GovBudg, PrivBudg) values (5, 18, 4);
+insert into ResearchCenter (ID_resCe, GovBudg, PrivBudg) values (6, 22, 37);
+insert into ResearchCenter (ID_resCe, GovBudg, PrivBudg) values (7, 78, 38);
+insert into ResearchCenter (ID_resCe, GovBudg, PrivBudg) values (8, 83, 45);
+insert into ResearchCenter (ID_resCe, GovBudg, PrivBudg) values (9, 55, 40);
+insert into ResearchCenter (ID_resCe, GovBudg, PrivBudg) values (10, 25, 1);
+insert into ResearchCenter (ID_resCe, GovBudg, PrivBudg) values (11, 33, 40);
+insert into ResearchCenter (ID_resCe, GovBudg, PrivBudg) values (12, 52, 5);
+insert into ResearchCenter (ID_resCe, GovBudg, PrivBudg) values (13, 62, 73);
+insert into ResearchCenter (ID_resCe, GovBudg, PrivBudg) values (14, 23, 74);
+insert into ResearchCenter (ID_resCe, GovBudg, PrivBudg) values (15, 43, 45);
---
+++ DDL/University.sql
@@ -0,0 +1,10 @@
+insert into university (id_university, govbudg) values (1, 98000);
+insert into university (id_university, govbudg) values (2, 50000);
+insert into university (id_university, govbudg) values (3, 72000);
+insert into university (id_university, govbudg) values (4, 68000);
+insert into university (id_university, govbudg) values (5, 57000);
+insert into university (id_university, govbudg) values (6, 75000);
+insert into university (id_university, govbudg) values (7, 38000);
+insert into university (id_university, govbudg) values (8, 52000);
+insert into university (id_university, govbudg) values (9, 57000);
+insert into university (id_university, govbudg) values (10, 75000);
